e826b25a434ab48f5350ea76fc40c40a3be9ef90
yokochi <yokochi@webnote.co.jp>
2022-05-08 11:55:47 +0900

    docs

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: './',
+    base: '/docs/',
     build: {
+		outDir: 'docs',
         cssCodeSplit: false,
     },
 })
